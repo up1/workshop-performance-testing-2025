f9d1178e96721e653f1ab9d762cd549c30cdfbf8
up1 <somkiat.p@gmail.com>
2025-06-29 01:16:49 +0700

    Add more api testing

--- k6/loadtest.js
+++ k6/loadtest.js
@@ -7,10 +7,19 @@ export let options = {
 };
 
 export default function () {
+    // Get all users
     let res = http.get('http://api-bad:8080/users');
     check(res, {
         'status is 200': (r) => r.status === 200,
         'body is not empty': (r) => r.body.length > 0,
     });
-    sleep(1);
+
+    // Get user by ID with random ID
+    let userId = Math.floor(Math.random() * 90000) + 1; // Assuming IDs are 1-90000
+    res = http.get(`http://api-bad:8080/users/${userId}`);
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'body is not empty': (r) => r.body.length > 0,
+    });
+
 }
